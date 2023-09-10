9027e736cb0eeffb8735ef7ebf785e399f980381
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-09-10 10:18:30 +0700

    init state

---
+++ flutter_quiz_app/lib/QuestionBackground.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuestionBackground extends StatefulWidget {
+  const QuestionBackground({super.key});
+
+  @override
+  State<QuestionBackground> createState() {
+    return _QuestionBackgroundState();
+  }
+}
+
+class _QuestionBackgroundState extends State<QuestionBackground> {
+  @override
+  Widget build(context) {
+    return const Text('QuestionsScreen');
+  }
+}
---
+++ flutter_quiz_app/lib/quiz.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz_app/QuestionBackground.dart';
+import 'package:flutter_quiz_app/StartBackground.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartBackground(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionBackground();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
