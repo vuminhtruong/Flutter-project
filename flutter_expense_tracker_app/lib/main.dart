86238d1eac622f3764606ce24f1aea8f1f05cd3a
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-09-11 20:28:29 +0700

    git add project

---
+++ flutter_expense_tracker_app/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_expense_tracker_app/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
