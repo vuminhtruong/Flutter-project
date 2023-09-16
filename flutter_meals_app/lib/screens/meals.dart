7220fd6994d2095a36a22e580c86b4fe8ba8c118
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-09-16 09:53:53 +0700

    add meals screen

--- flutter_meals_app/lib/screens/meals.dart
+++ flutter_meals_app/lib/screens/meals.dart
@@ -10,36 +10,36 @@ class MealsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget content = Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          const Text('Uh oh ...  nothing here!'),
-          const SizedBox(
-            height: 16,
-          ),
-          Text(
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+            const Text('Uh oh ...  nothing here!'),
+        const SizedBox(
+          height: 16,
+        ),
+        Text(
             'Try selecting a different category!',
-            style: Theme.of(context)
+            style: Theme
+                .of(context)
                 .textTheme
-                .headlineLarge
-                ?.copyWith(color: Theme.of(context).colorScheme.onBackground),
-          )
-        ],
-      ),
+                .bodyLarge?.copyWith(color: Theme.of(context).colorScheme.primary)
+    ,)
+    ],
+    ),
     );
 
     if (meals.isNotEmpty) {
-      content = ListView.builder(
-        itemCount: meals.length,
-        itemBuilder: (ctx, index) => Text(meals[index].title),
-      );
+    content = ListView.builder(
+    itemCount: meals.length,
+    itemBuilder: (ctx, index) => Text(meals[index].title,),
+    );
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: content,
+    appBar: AppBar(
+    title: Text(title),
+    ),
+    body: content,
     );
   }
 }
