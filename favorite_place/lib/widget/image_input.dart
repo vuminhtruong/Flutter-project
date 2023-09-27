269b3ef9e9e19e127c9887cb8bf693a9fac26c64
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-09-27 18:06:49 +0700

    use device camera

--- favorite_place/lib/widget/image_input.dart
+++ favorite_place/lib/widget/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,27 +13,56 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {
+  File? _selectedImage;
 
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+    await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) {
+      return;
+    }
+
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      onPressed: _takePicture,
+      icon: const Icon(Icons.camera_alt),
+      label: const Text('Take Picture'),
+    );
+
+    if (_selectedImage != null) {
+      content = GestureDetector(
+          onTap: _takePicture,
+          child: Image.file(
+            _selectedImage!,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: double.infinity,
+          )
+      );
+    }
+
     return Container(
       decoration: BoxDecoration(
-        border: Border.all(
-          width: 1,
-          color: Theme.of(context).colorScheme.primary.withOpacity(0.2),
-        )
-      ),
-      height: 200,
+          border: Border.all(
+            width: 1,
+            color: Theme
+                .of(context)
+                .colorScheme
+                .primary
+                .withOpacity(0.2),
+          )),
+      height: 240,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-        onPressed: _takePicture,
-        icon: const Icon(Icons.camera_alt),
-        label: const Text('Take Picture'),
-      ),
+      child: content,
     );
   }
 }
