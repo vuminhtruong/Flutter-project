b64d8b0977329d1d4c64b752859fa23066dab79a
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-09-28 11:20:22 +0700

    display place_detail

--- favorite_place/lib/screen/place_detail_screen.dart
+++ favorite_place/lib/screen/place_detail_screen.dart
@@ -7,6 +7,12 @@ class PlaceDetailScreen extends StatelessWidget {
 
   final Place place;
 
+  String get locationImage {
+    final lat = place.location.latitude;
+    final long = place.location.longitude;
+    return 'https://maps.googleapis.com/maps/api/staticmap?center=$lat,$long=&zoom=15&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C$lat,$long&key=AIzaSyA_eav17zJTNjJjuNMN7y8ErooxGqmIE74';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,6 +26,37 @@ class PlaceDetailScreen extends StatelessWidget {
               fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Column(
+                children: [
+                  CircleAvatar(radius: 50,backgroundImage: NetworkImage(locationImage),),
+                  Container(
+                    alignment: Alignment.center,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16,
+                    ),
+                    decoration: const BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.transparent,
+                        Colors.white,
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      ),
+                    ),
+                    child: Text(
+                      place.location.address,
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground),),
+                  )
+                ],
+              ),
             )
           ],
         ));
--- favorite_place/lib/widget/places_list.dart
+++ favorite_place/lib/widget/places_list.dart
@@ -33,10 +33,14 @@ class PlacesList extends StatelessWidget {
                 style: Theme.of(context).textTheme.titleMedium!.copyWith(
                     color: Theme.of(context).colorScheme.onBackground),
               ),
+              subtitle: Text(
+                places[index].location.address,
+                style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground),
+              ),
               onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (ctx) => PlaceDetailScreen(place: places[index]))
-                );
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (ctx) => PlaceDetailScreen(place: places[index])));
               },
             ));
   }
