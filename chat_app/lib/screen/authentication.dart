57fa674c9fa64fe26feb1894f6e0aaf81dac600c
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-09-30 15:37:17 +0700

    add modes authentication

--- chat_app/lib/screen/authentication.dart
+++ chat_app/lib/screen/authentication.dart
@@ -11,6 +11,7 @@ class AuthenticationScreen extends StatefulWidget {
 }
 
 class _AuthenticationScreenState extends State<AuthenticationScreen> {
+  var _isLogin = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,6 +53,24 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
                               labelText: 'Password',
                             ),
                             obscureText: true,
+                          ),
+                          const SizedBox(height: 12,),
+                          ElevatedButton(
+                              onPressed: () {
+
+                              },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context).colorScheme.primaryContainer
+                            ),
+                              child: Text(_isLogin ? 'Login' : 'Sign Up'),
+                          ),
+                          TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(_isLogin ? 'Create an account' : 'I already have an account'),
                           )
                         ],
                       ),
@@ -65,5 +84,5 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
       ),
     );
   }
-  
+
 }
