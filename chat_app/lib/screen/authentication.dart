431023621a4547a19a92d6a078011907d83a7fb1
vuminhtruong <vuminhtruongvmu18@gmail.com>
2023-10-02 11:16:41 +0700

    send data to firestore_database

--- chat_app/lib/screen/authentication.dart
+++ chat_app/lib/screen/authentication.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:chat_app/widget/user_image_picker.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
@@ -52,6 +53,12 @@ class _AuthenticationScreenState extends State<AuthenticationScreen> {
             .child('${userCredentials.user!.uid}.jpg');
         await storageRef.putFile(_selectedImage!);
         final imageUrl = await storageRef.getDownloadURL();
+
+        FirebaseFirestore.instance.collection('users').doc(userCredentials.user!.uid).set({
+          'username' : 'to be done...',
+          'email' : _enteredEmail,
+          'image_url' : imageUrl,
+        });
       }
     } on FirebaseAuthException catch (error) {
       if (error.code == 'email-already-in-use') {}
